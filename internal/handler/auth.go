4f912c083097cd3e9ddb79a97e4c864bb9b61b3b
aolzhass <aolzhass@L02R01S03s-iMac.local>
2024-03-15 01:05:26 +0500

    edit error response

--- internal/handler/auth.go
+++ internal/handler/auth.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"net/http"
 	"todoApp/internal/models"
 
 	"github.com/gin-gonic/gin"
@@ -10,7 +11,8 @@ func (h *Handler) signUp(c *gin.Context) {
 	var user models.User
 
 	if err := c.BindJSON(&user); err != nil {
-		
+		newErrorResponse(c, http.StatusBadRequest, err.Error(), h.log)
+		return
 	}
 }
 
--- internal/handler/reponse.go
+++ internal/handler/reponse.go
@@ -1,13 +1,16 @@
 package handler
 
 import (
+	"todoApp/pkg/logger"
+
 	"github.com/gin-gonic/gin"
 )
 
-type error struct {
-	Message string `json: message`
+type errorResponse struct {
+	Message string `json:"message"`
 }
 
-func newErrorResponse(c *gin.Context, statusCode int, message string) {
-	c.AbortWithStatusJSON(statusCode, message)
+func newErrorResponse(c *gin.Context, statusCode int, message string, log logger.Logger) {
+	log.Error(message)
+	c.AbortWithStatusJSON(statusCode, errorResponse{message})
 }
