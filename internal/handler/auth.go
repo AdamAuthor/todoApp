230f3f1f3f4ee367de7909a43819cdcf60b5c538
aolzhass <aolzhass@L02R01S03s-iMac.local>
2024-03-15 21:15:08 +0500

    refactoe models

--- internal/handler/auth.go
+++ internal/handler/auth.go
@@ -7,20 +7,19 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type errorResponse struct {
-	Message string `json:"message"`
-}
-
 func (h *Handler) signUp(c *gin.Context) {
 	var user models.User
 
 	if err := c.BindJSON(&user); err != nil {
-		newErrorResponse(c, http.StatusBadRequest, err.Error(), h.log)
+		h.log.Error(err.Error())
+		c.AbortWithStatusJSON(http.StatusBadRequest, models.ErrorResponse{Message: err.Error()})
 		return
 	}
 	id, err := h.service.Authorization.CreateUser(user)
 	if err != nil {
-		newErrorResponse(c, http.StatusInternalServerError, err.Error(), h.log)
+		h.log.Error(err.Error())
+		c.AbortWithStatusJSON(http.StatusInternalServerError, models.ErrorResponse{Message: err.Error()})
+		return
 	}
 
 	c.JSON(http.StatusOK, map[string]interface{}{
@@ -28,23 +27,18 @@ func (h *Handler) signUp(c *gin.Context) {
 	})
 }
 
-type signInInput struct {
-	Username string `json:"username"  binding:"required"`
-	Password string `json:"password"  binding:"required"`
-}
-
 func (h *Handler) signIn(c *gin.Context) {
-	var signIn signInInput
+	var signIn models.SignInInput
 
 	if err := c.BindJSON(&signIn); err != nil {
 		h.log.Error(err.Error())
-		c.AbortWithStatusJSON(http.StatusBadRequest, errorResponse{err.Error()})
+		c.AbortWithStatusJSON(http.StatusBadRequest, models.ErrorResponse{Message: err.Error()})
 		return
 	}
 	token, err := h.service.Authorization.GenerateToken(signIn.Username, signIn.Password)
 	if err != nil {
 		h.log.Error(err.Error())
-		c.AbortWithStatusJSON(http.StatusInternalServerError, errorResponse{err.Error()})
+		c.AbortWithStatusJSON(http.StatusInternalServerError, models.ErrorResponse{Message: err.Error()})
 		return
 	}
 
